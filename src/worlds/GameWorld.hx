01694098988c37fe61455f901c4f4abcbd6b0fb8
jasonhutchens <jason@agworld.com.au>
2013-01-26 02:45:51 +0800

    refactor out into a separate class

--- src/worlds/GameWorld.hx
+++ src/worlds/GameWorld.hx
@@ -1,5 +1,5 @@
 package worlds;
- 
+
 import haxe.Json;
 
 import cpp.vm.Thread;
@@ -9,26 +9,98 @@ import com.haxepunk.World;
 import com.haxepunk.utils.Input;
 import com.haxepunk.graphics.Text;
 import com.haxepunk.Entity;
- 
+
+class PubNub
+{
+  private var _pub_thread:Thread;
+  private var _sub_thread:Thread;
+
+  public function new(pub_key, sub_key, channel)
+  {
+    var service = "http://pubsub.pubnub.com";
+    _pub_thread = _create_publisher(service+"/publish/"+pub_key+"/"+sub_key+"/0/"+channel+"/0/");
+    _sub_thread = _create_subscriber(service+"/subscribe/"+sub_key+"/"+channel+"/0/");
+  }
+
+  public function send(object):Void
+  {
+    _pub_thread.sendMessage(object);
+  }
+
+  public function read():Dynamic
+  {
+    return Thread.readMessage(false);
+  }
+
+  static private function _create_publisher(base):Thread
+  {
+    var thread = Thread.create(function():Void {
+      var base = Thread.readMessage(true);
+      while(true) {
+        var object = Thread.readMessage(true);
+        var url = base + Json.stringify(object);
+        var client = new haxe.Http(url);
+        client.onError = function(error) {
+          trace("PUB ERROR : " + Std.string(error));
+        };
+        client.onData = function(data) {
+          // NOOP
+        };
+        client.request(false);
+      }
+    });
+    thread.sendMessage(base);
+    return thread;
+  }
+
+  static private function _create_subscriber(base):Thread
+  {
+    var thread = Thread.create(function():Void {
+      var main:Thread = Thread.readMessage(true);
+      var base = Thread.readMessage(true);
+      var time_token = "0";
+      while(true) {
+        var url = base + time_token;
+        var client = new haxe.Http(url);
+        client.onError = function(error) {
+          trace("SUB ERROR : " + Std.string(error));
+        };
+        client.onData = function(data) {
+          var object = Json.parse(data);
+          time_token = object[1];
+          main.sendMessage(object[0]);
+        };
+        client.request(false);
+      }
+    });
+    thread.sendMessage(Thread.current());
+    thread.sendMessage(base);
+    return thread;
+  }
+}
+
 class GameWorld extends World
 {
+  private var _counter:Int;
+  private var _pubnub:PubNub;
   private var _messageImage:Text;
   private var _messageEntity:Entity;
-  private var _sub_channel:Thread;
-  private var _pub_channel:Thread;
-  private var _counter:Int;
 
   public function new()
   {
     super();
 
-    _pub_channel = start_pub("kranzky");
-    _sub_channel = start_sub("kranzky");
+    _counter = 1;
+
+    var pub_key = "pub-c-0834dc19-81c6-4378-9ab7-db3d457d9472";
+    var sub_key = "sub-c-132a21ec-66ec-11e2-903d-12313f022c90";
+    var channel = "kranzky";
+    _pubnub = new PubNub(pub_key, sub_key, channel);
 
     _messageImage = new Text("Hello");
-    _messageEntity = new Entity(HXP.screen.width/2, HXP.screen.height/2, _messageImage);
-
-    _counter = 1;
+    var x = HXP.screen.width/2;
+    var y = HXP.screen.height/2;
+    _messageEntity = new Entity(x, y, _messageImage);
   }
 
   public override function begin()
@@ -44,68 +116,12 @@ class GameWorld extends World
       } else {
         _messageImage.text = "Hello";
       }
-      _pub_channel.sendMessage("hello-from-" + Sys.systemName() + "-" + _counter);
+      _pubnub.send("hello-from-" + Sys.systemName() + "-" + _counter);
       _counter += 1;
     }
-    var response = Thread.readMessage(false);
+    var response = _pubnub.read();
     if (response != null) {
       trace("READ: " + response);
-    } 
-  }
-
-  //------------------------------------------------------------------------------
-
-  public function start_pub(channel):Thread
-  {
-    var thread = Thread.create(function():Void {
-      var main:Thread = Thread.readMessage(true);
-      var channel = Thread.readMessage(true);
-
-      while(true) {
-        var message = Thread.readMessage(true);
-        var URL = "http://pubsub.pubnub.com/publish/pub-c-0834dc19-81c6-4378-9ab7-db3d457d9472/sub-c-132a21ec-66ec-11e2-903d-12313f022c90/0/" + channel + "/0/" + Json.stringify(message);
-        var client = new haxe.Http(URL);
-        client.onError = function(error) {
-          trace("PUB ERROR : " + Std.string(error));
-        };
-        client.onData = function(data) {
-          // NOOP
-        };
-        client.request(false);
-      }
-    });
-
-    thread.sendMessage(Thread.current());
-    thread.sendMessage(channel);
-
-    return thread;
-  }
-
-  public function start_sub(channel):Thread
-  {
-    var thread = Thread.create(function():Void {
-      var main:Thread = Thread.readMessage(true);
-      var channel = Thread.readMessage(true);
-      var time_token = "0";
-
-      while(true) {
-        var url = "http://pubsub.pubnub.com/subscribe/sub-c-132a21ec-66ec-11e2-903d-12313f022c90/" + channel + "/0/" + time_token;
-        var client = new haxe.Http(url);
-        client.onError = function(error) {
-          trace("SUB ERROR : " + Std.string(error));
-        };
-        client.onData = function(body) {
-          var data = Json.parse(body);
-          time_token = data[1];
-          main.sendMessage(data[0]);
-        };
-        client.request(false);
-      }
-    });
-
-    thread.sendMessage(Thread.current());
-    thread.sendMessage(channel);
-
-    return thread;
+    }
   }
 }
